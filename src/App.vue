ed0a40b8c975a752beaa4df57b18899b42c595d2
Mohsen Pakzad <44529676+mohsenpakzad@users.noreply.github.com>
2021-07-15 05:15:12 +0430

    Pull off app navigator and store it in a separate component

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,20 @@
 <template>
   <section>
-    <b-card no-body class="mx-2 mb-3">
-      <b-row class="justify-content-between align-items-center">
-        <b-col cols="auto">
-          <b-button @click="$router.back()" variant="light" style="outline: none; box-shadow: none;">
-            <b-icon icon="arrow-left"></b-icon>
-          </b-button>
-        </b-col>
-
-        <b-col cols="auto">
-          <h5 class="font-weight-light" style="margin-top: 5px;">
-            Current Route: {{ this.$route.fullPath }} - Route Name: {{ this.$route.name }}
-          </h5>
-        </b-col>
-
-        <b-col cols="auto">
-          <b-button @click="$router.forward()" variant="light" style="outline: none; box-shadow: none;">
-            <b-icon icon="arrow-right"></b-icon>
-          </b-button>
-        </b-col>
-      </b-row>
-    </b-card>
-
-    <b-card class="mx-2">
-      <router-view></router-view>
-    </b-card>
-
+    <app-navigator></app-navigator>
+    <router-view></router-view>
   </section>
 </template>
 
 <script>
 // for sample
 // import { getAllUsers, insertUser } from '@/db/user/users'
+import AppNavigator from '@/components/AppNavigator'
 
 export default {
   name: 'app',
+  components: {
+    AppNavigator
+  },
   // async created() {
   //   getAllUsers().then((data) =>{
   //     console.log(data)
---
+++ src/components/AppNavigator.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-card no-body>
+    <b-row class="justify-content-between align-items-center">
+      <b-col cols="auto">
+        <b-button @click="$router.back()" variant="light" style="outline: none; box-shadow: none;">
+          <b-icon icon="arrow-left"></b-icon>
+        </b-button>
+      </b-col>
+
+      <b-col cols="auto">
+        <h5 class="font-weight-light" style="margin-top: 5px;">
+          Current Route: {{ this.$route.fullPath }} - Route Name: {{ this.$route.name }}
+        </h5>
+      </b-col>
+
+      <b-col cols="auto">
+        <b-button @click="$router.forward()" variant="light" style="outline: none; box-shadow: none;">
+          <b-icon icon="arrow-right"></b-icon>
+        </b-button>
+      </b-col>
+    </b-row>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'AppNavigator'
+}
+</script>
