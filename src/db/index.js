002df52e5845159de22b5da96822e027976e6e20
shima mohammadpour <shi1999mamp@email.com>
2021-07-16 13:26:56 +0430

    index.js completed

--- src/db/index.js
+++ src/db/index.js
@@ -64,7 +64,7 @@ export async function createTables() {
     await createInvitationsTable()
     await createConnectionsTable()
 
-    // posting tables
+    // posting tables 
     await createPostsTable()
     await createCommentsTable()
     await createPostLikesTable()
