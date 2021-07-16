ec75dfc0a6925cd3777ef103fffd98f5d158b2ac
shima mohammadpour <shi1999mamp@email.com>
2021-07-16 13:27:50 +0430

    user profile and user vue added

---
+++ src/views/user/UserProfile.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <p>Loaded Id: {{ user.id }} </p>
+    <p>Loaded username: {{ user.username }} </p>
+  </section>
+</template>
+
+<script>
+import { getUserById } from '@/db/user/users'
+
+export default {
+  name: 'UserProfile',
+  data: () => ({
+    user: {
+      id: '',
+      username: '',
+    }
+  }),
+  async mounted() {
+    this.user = await getUserById(this.$route.params.userId)
+  }
+}
+</script>
