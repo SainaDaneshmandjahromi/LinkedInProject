54ae74a71c963b6df11e3e575427d6fb58858a2f
Mohsen Pakzad <44529676+mohsenpakzad@users.noreply.github.com>
2021-07-20 00:05:37 +0430

    Add picking user avatar functionality to signup and polish login and signup code logic

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,43 +1,42 @@
 <template>
-  <section>
-    <b-card class="mx-5 mt-3">
+  <b-card class="mx-5 mt-3">
 
-      <label>Username:</label>
-      <b-form-input
-        v-model="user.username"
-        type="text"
-        trim
-      ></b-form-input>
+    <label>Username:</label>
+    <b-form-input
+      v-model="user.username"
+      type="text"
+      trim
+    ></b-form-input>
 
-      <label class="mt-2">Password:</label>
-      <b-form-input
-        v-model="user.password"
-        type="password"
-        trim
-      ></b-form-input>
+    <label class="mt-2">Password:</label>
+    <b-form-input
+      v-model="user.password"
+      type="password"
+      trim
+    ></b-form-input>
 
-      <label class="mt-2">Birthday:</label>
-      <b-form-input
-        v-model="user.birthday"
-        type="date"
-        trim
-      ></b-form-input>
+    <label class="mt-2">Birthday:</label>
+    <b-form-input
+      v-model="user.birthday"
+      type="date"
+      trim
+    ></b-form-input>
 
-      <b-button
-        :disabled="allowSignup"
-        block
-        class="mt-3"
-        variant="success"
-        @click="signup"
-      >Signup</b-button>
+    <b-button
+      :disabled="disallowSignup"
+      block
+      class="mt-3"
+      variant="success"
+      @click="signup"
+    >
+      Signup
+    </b-button>
 
-    </b-card>
-
-  </section>
+  </b-card>
 </template>
 
 <script>
-import { insertUser, getUserByUsername } from '@/db/user/users'
+import { getUserByUsername, insertUser } from '@/db/user/users'
 
 export default {
   name: 'Signup',
@@ -45,16 +44,30 @@ export default {
     user: {
       username: '',
       password: '',
-      birthday: ''
-    }
+      avatar: '',
+      birthday: '',
+    },
+    avatars: [
+      ':)',
+      '^-^',
+      '-_-',
+      'O_o',
+      'XD',
+      ':D',
+      '>.<',
+      ':P'
+    ]
   }),
   methods: {
-    async signup(){
+    async signup() {
       const takenUser = await getUserByUsername(this.user.username)
-      if (takenUser){
+      if (takenUser) {
         console.log('this username is already taken')
         return
       }
+      this.user.username = this.user.username.toLowerCase()
+      this.user.avatar = this.avatars[Math.floor(Math.random() * this.avatars.length)]
+      if (this.user.birthday === '') this.user.birthday = null
 
       await insertUser(this.user)
       const user = await getUserByUsername(this.user.username)
@@ -62,7 +75,7 @@ export default {
     }
   },
   computed: {
-    allowSignup() {
+    disallowSignup() {
       return this.user.username === '' || this.user.password === ''
     }
   },
