f1bd5e2684e1baa18c469046acf28680fd9a72d4
shima mohammadpour <shi1999mamp@email.com>
2021-07-22 01:21:59 +0430

    user notifications

--- src/views/user/UserNotifications.vue
+++ src/views/user/UserNotifications.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
-    User notifications
+    <div :key="notif.id" v-for="notif in notifs">
+      <template v-if="notif.type === TYPE_POST_LIKE>
+        {{notif.transmitter}} liked your post 
+
+      </template>      
+    </div>
   </div>
 </template>
 
 <script>
+import { getUnreadNotificationByReceiverUserId,
+TYPE_BIRTHDAY,
+ TYPE_PROFILE_SEEN,
+ TYPE_POST_LIKE,
+TYPE_POST_COMMENT ,
+TYPE_COMMENT_LIKE,  
+ TYPE_COMMENT_REPLAY , 
+ TYPE_ENDORSE ,
+ TYPE_CHANGE_JOB_POSITION } from '@/db/user/notifications'
 export default {
-  name: 'UserNotifications'
+  name: 'UserNotifications',
+   data: () => ({
+    
+    notifs : [{
+      transmitter:'folani ' 
+    }]
+  }),
+  async created() {
+      
+      //this.notifs = await  getUnreadNotificationByReceiverUserId(this.$route.params.userId)
+      
+  }
+  
 }
 </script>
